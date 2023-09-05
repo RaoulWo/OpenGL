5fcaa6a307e8574d79c18cf5b15567bd644ff15d
Raoul Wograndl <90349386+RaoulWo@users.noreply.github.com>
2023-09-05 23:45:17 +0200

    Include GLEW (OpenGL Extension Wrangler Library)

--- OpenGL/src/main.cc
+++ OpenGL/src/main.cc
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void) {
   GLFWwindow* window;
@@ -16,6 +19,11 @@ int main(void) {
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
+
+  if (glewInit() != GLEW_OK)
+    std::cout << "GLEW initialization error!" << std::endl;
+
+  std::cout << glGetString(GL_VERSION) << std::endl;
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
